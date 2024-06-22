0dc11ed8ad38d9c89d84cd9f1501458bea5e14f8
Bodechi <Fosa9301@mail.ru>
2024-06-22 22:28:28 +0300

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,13 @@
+#my_dict = {"Andrey": 31, "Ivan": 24, "Maxim": 43}
+#print(my_dict["Andrey"])
+#print(my_dict.get("Andrey"))
+#print(my_dict.get("Max"))
+#my_dict["Jone"] = 41
+#my_dict["Kiano"] = 60
+#del (my_dict["Ivan"])
+#print(my_dict)'
+my_set = {1, 5, 6, 4, 1, 6, 4, 3, 5, 3, "cod", "imeet", "smisl", "esli", "smisl;", "imeet", "cod"}
+print(my_set)
+my_set.add("a moget i net")
+my_set.add(33)
+print(my_set)
