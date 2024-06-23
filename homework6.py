96a5c8cc36fc516f2f50e36330e82310c81d831f
Bodechi <Fosa9301@mail.ru>
2024-06-23 17:47:10 +0300

    Update homework6.py
    my_dict.pop("Andrey") Заменил

--- homework6.py
+++ homework6.py
@@ -4,7 +4,7 @@
 #print(my_dict.get("Max"))
 #my_dict["Jone"] = 41
 #my_dict["Kiano"] = 60
-#del (my_dict["Ivan"])
+#my_dict.pop("Andrey")
 #print(my_dict)'
 my_set = {1, 5, 6, 4, 1, 6, 4, 3, 5, 3, "cod", "imeet", "smisl", "esli", "smisl;", "imeet", "cod"}
 print(my_set)
